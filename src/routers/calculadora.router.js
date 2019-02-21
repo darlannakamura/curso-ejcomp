9911875a63756d0f736623ec78c6e3dc7fa0b23d
Vulpes <fccs1998@gmail.com>
2019-02-21 19:10:26 -0300

    Revert "Revert "adicionando func soma""

--- src/routers/calculadora.router.js
+++ src/routers/calculadora.router.js
@@ -4,6 +4,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/calculadora.controller');
 
-// router.post('/', controller.soma);
+router.post('/soma', controller.soma);
 
 module.exports = router;
