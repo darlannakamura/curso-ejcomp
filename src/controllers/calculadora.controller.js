faf84224e703bd9a3789984a3120293bbf22d682
BrenoMaia98 <brenomaia98@gmail.com>
2019-02-21 18:35:29 -0300

    Criada a função multiplicação

--- src/controllers/calculadora.controller.js
+++ src/controllers/calculadora.controller.js
@@ -3,3 +3,13 @@
 exports.soma = async(req, res, next) => {
         
 }
+
+
+exports.multiplicacao = async(req, res, next) => {
+	const a = req.body.a;
+	const b = req.body.b;
+	var x = a*b; 
+	res.status(200).send({
+		resultado: x
+	});
+}
